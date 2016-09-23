f1dcad252cea01cf81d7c7da198b4ddd598c510b
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-23 08:34:21 -0700

    Add Save, GetAll and Delete  Methods and Test on Stylist

--- src/Stylist.php
+++ src/Stylist.php
@@ -22,26 +22,41 @@
 
         function getName()
         {
-
+            return $this->name;
         }
 
-        function setName()
+        function setName($new_name)
         {
-
+            $this->name = $new_name;
         }
 
 //Regular Methods
-
-
-
-
-
+        function save()
+        {
+            $GLOBALS['DB']->exec(
+            "INSERT INTO stylists (name) VALUES ('{$this->getName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
 
 
 //Static Methods
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+            $stylists = array();
+            foreach($returned_stylists as $stylist) {
+                $id = $stylist['id'];
+                $name = $stylist['name'];
+                $new_stylist = new Stylist($id, $name);
+                array_push($stylists, $new_stylist);
+            }
+            return $stylists;
+        }
 
-
-
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists");
+        }
 
 
 
--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -11,12 +11,13 @@
 
     class StylistTest extends PHPUnit_Framework_TestCase
     {
-    //     protected function tearDown()
-    //    {
-    //        FirstClass::deleteAll();
-    //        SecondClass::deleteAll();
-    //    }
+        protected function tearDown()
+       {
+            Stylist::deleteAll();
+            // SecondClass::deleteAll();
+       }
 
+//Test Getters and Setters
        function test_getId()
        {
            //Arrange
@@ -31,7 +32,94 @@
            $this->assertEquals($id, $result);
        }
 
+       function test_getName()
+       {
+           //Arrange
+           $id = 1;
+           $name = "Bilbo Baggins";
+           $test_stylist = new Stylist($id, $name);
+
+           //Act
+           $result = $test_stylist->getName();
+
+           //Assert
+           $this->assertEquals($name, $result);
+       }
+
+       function test_setName()
+       {
+           //Arrange
+           $id = 1;
+           $name = "Bilbo Baggins";
+           $test_stylist = new Stylist($id, $name);
+           $new_name = "Frodo Baggins";
+
+           //Act
+           $test_stylist->setName($new_name);
+           $result = $test_stylist->getName();
+
+           //Assert
+           $this->assertEquals($new_name, $result);
+       }
+
+//Test Methods
+        function test_save()
+        {
+            //Arrange
+            $id = null;
+            $name = "Bilbo Baggins";
+            $test_stylist = new Stylist($id, $name);
+            $test_stylist->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals($test_stylist, $result[0]);
+        }
+
+//Test Static Methods
+        function test_getAll()
+        {
+            //Arrange
+            $id1 = null;
+            $name1 = "Bilbo Baggins";
+            $test_stylist1 = new Stylist($id1, $name1);
+            $test_stylist1->save();
+            $id2 = null;
+            $name2 = "Frodo Baggins";
+            $test_stylist2 = new Stylist($id2, $name2);
+            $test_stylist2->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([$test_stylist1, $test_stylist2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $id1 = null;
+            $name1 = "Bilbo Baggins";
+            $test_stylist1 = new Stylist($id1, $name1);
+            $test_stylist1->save();
+            $id2 = null;
+            $name2 = "Frodo Baggins";
+            $test_stylist2 = new Stylist($id2, $name2);
+            $test_stylist2->save();
+
+            //Act
+            Stylist::deleteAll();
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
 
 
+
+//End Unit Test
    }
 ?>
