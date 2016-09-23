0ebf13d83ca462ab1cb1bbef3cf1bda6272f6f8d
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-23 13:12:21 -0700

    Add Client Data Pages

--- app/app.php
+++ app/app.php
@@ -21,7 +21,7 @@
     });
 
 
-//Stylist Path
+//Stylist Paths
     $app->get("/stylists", function() use ($app) {
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
     });
@@ -32,6 +32,28 @@
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+    $app->get("/stylists/{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+
+//Client Paths
+    $app->get("/clients", function() use ($app) {
+        return $app['twig']->render('clients.html.twig', array('tasks' => Client::getAll()));
+    });
+
+    $app->post("/clients", function() use ($app) {
+        $id = null;
+        $name = $_POST['name'];
+        $stylist_id = $_POST['stylist_id'];
+        $client = new Client($id, $name, $stylist_id);
+        $client->save();
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+
+
+//End Paths
 
     return $app;
 
